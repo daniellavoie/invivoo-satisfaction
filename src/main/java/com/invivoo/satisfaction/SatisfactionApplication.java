813e81a6a0f2a4e0889dd723613fe51fb8cafa4d
Daniel Lavoie <dlavoie@live.ca>
2016-10-16 11:34:57 +0200

    Ajout du service consultant.

---
+++ src/main/java/com/invivoo/satisfaction/SatisfactionApplication.java
@@ -0,0 +1,11 @@
+package com.invivoo.satisfaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatisfactionApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SatisfactionApplication.class, args);
+	}
+}
---
+++ src/main/java/com/invivoo/satisfaction/service/ConsultantService.java
@@ -0,0 +1,9 @@
+package com.invivoo.satisfaction.service;
+
+import java.util.List;
+
+import com.invivoo.satisfaction.model.Consultant;
+
+public interface ConsultantService {
+	List<Consultant> findAll();
+}
